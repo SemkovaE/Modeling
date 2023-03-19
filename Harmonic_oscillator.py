d9c7a95b37988124763bbc22cc70a27bf6e621e4
SemkovaE <125797080+SemkovaE@users.noreply.github.com>
2023-03-19 19:06:32 +0500

    Add files via upload

--- Harmonic_oscillator.py
+++ Harmonic_oscillator.py
@@ -1,20 +1,13 @@
-from numpy import sin
+from numpy import sin, arange
 import matplotlib.pyplot as plt
 
 A = 1 # Амплитуда
 w = 1 # Частота колебания
-t = 0 # Время
-dt = 0.01 # Шаг
 fi = 0 # Начальная фаза
 
-def osc(A, w, t, dt, fi):
-    row_t = []
-    row_x = []
-    while t <= 10:
-        x = A * sin(w * t + fi)
-        row_t.append(t)
-        row_x.append(x)
-        t += dt
+def osc(A, w, fi):
+    row_t = arange(0, 10, 0.01)
+    row_x = A * sin(w * row_t + fi)
     return row_t, row_x
 
 def osc_plot(row_t, row_x):
@@ -26,7 +19,7 @@ def osc_plot(row_t, row_x):
     plt.text(0, -1, 'A = {}; {} = {}; {} = {}'.format(A, chr(969), w, chr(966), fi), fontsize = 10)
     plt.grid(True)
 
-row_t, row_x = osc(A, w, t, dt, fi)
+row_t, row_x = osc(A, w, fi)
 osc_plot(row_t, row_x)
 
 plt.show()
